d747c132be6c453c5d6a16629c7f93521aa4e251
Hai Do <dodinhhai29904@gmail.com>
2023-07-16 10:37:06 +0700

    Update selection sort

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,2 +1,17 @@
 #include "lib.h"
 
+void selectionSort(int arr[], int size){
+    for(int i = 0; i < size - 1; i++){
+        int min = arr[i];
+        int minIndex = i;
+        for(int j = i + 1; j < size; j++){
+            if(min > arr[j]){
+                minIndex = j;
+                min = arr[j];
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
